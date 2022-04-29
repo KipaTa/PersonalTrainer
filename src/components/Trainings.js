d51686faff031ed4542f234a28eb834e83cb5094
KipaTa <kirsi.tainio@gmail.com>
2022-04-29 14:07:19 +0300

    Delete training

--- src/components/Trainings.js
+++ src/components/Trainings.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import'ag-grid-community/dist/styles/ag-grid.css';
 import'ag-grid-community/dist/styles/ag-theme-material.css';
+import Button from '@mui/material/Button'
+import Snackbar from '@mui/material/Snackbar';
 
 export default function Customers() {
     const [trainings, setTrainings] = useState([]);
+    const [open, setOpen] = React.useState(false);
 
     
 useEffect(() => fetchData(), []);
@@ -15,7 +18,22 @@ const fetchData = () => {
    .then(data => setTrainings(data))
 }
 
+const deleteTraining= (id) => {
+    if (window.confirm('Are you sure?')) {
+      fetch("https://customerrest.herokuapp.com/api/trainings/" + id, {method: 'DELETE'})
+      .then (res => fetchData() )
+      .catch(err => console.error(err))
+      setOpen(true);
+    }
+  }
+
+const handleClose = () => {
+    setOpen(false);
+   };
+
 const columns = [
+    { width: 100, headerName: '', field:'links.0.href', cellRenderer:row=> <Button 
+        variant="contained" size="small" color="secondary" onClick={()=> deleteTraining(row.data.id)}>Delete</Button>},
     { field: 'date', sortable: true, filter: true, width: 180, cellRenderer: (data) => {
         return data ? (new Date(data.value)).toLocaleDateString() : '';  } },
     { field: 'duration', sortable: true, filter: true, width: 180   },
@@ -36,7 +54,14 @@ const columns = [
             columnDefs={columns} 
             rowData={trainings}>
         </AgGridReact>
-
+        <div>
+        <Snackbar
+          open={open}
+          autoHideDuration={6000}
+          onClose={handleClose}
+          message="Training deleted"
+        />
+      </div>
     
     </div>
     </div>

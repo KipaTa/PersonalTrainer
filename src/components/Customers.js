1cd28d94f4836e0b6ca6e74e69aa78cb26f80b0d
KipaTa <kirsi.tainio@gmail.com>
2022-04-29 11:21:24 +0300

    Delete Customer

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import'ag-grid-community/dist/styles/ag-grid.css';
 import'ag-grid-community/dist/styles/ag-theme-material.css';
+import Button from '@mui/material/Button'
+import Snackbar from '@mui/material/Snackbar';
 import AddCustomer from './AddCustomer';
 import EditCustomer from './EditCustomer';
 
 export default function Customers() {
     const [customers, setCustomers] = useState([]);
+    const [open, setOpen] = React.useState(false);
     
     
 useEffect(() => fetchData(), []);
@@ -41,6 +44,19 @@ const updateCustomer = (customer, link) => {
     .catch(err => console.error(err))
  }
 
+ const deleteCustomer = (link) => {
+    if (window.confirm('Are you sure?')) {
+      fetch(link, {method: 'DELETE'})
+      .then (res => fetchData() )
+      .catch(err => console.error (err))
+      setOpen(true);
+    }
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+   };
+
 const columns = [
     { field: 'firstname', sortable: true, filter: true, width: 180 },
     { field: 'lastname', sortable: true, filter: true, width: 180   },
@@ -49,7 +65,8 @@ const columns = [
     { field: 'city', sortable: true, filter: true, width: 180   },
     { field: 'email', sortable: true, filter: true, width: 180  },
     { field: 'phone', sortable: true, filter: true, width: 180  },
-    { width: 100, cellRenderer:row =><EditCustomer updateCustomer={updateCustomer} customer={row.data} /> }
+    { width: 100, cellRenderer:row =><EditCustomer updateCustomer={updateCustomer} customer={row.data} /> },
+    { width: 100, headerName: '', field:'links.0.href', cellRenderer:row=> <Button variant="contained" size="small" color="secondary" onClick={()=> deleteCustomer(row.value)}>Delete</Button>}
   ];
 
   return (
@@ -63,7 +80,14 @@ const columns = [
             columnDefs={columns} 
             rowData={customers}>
         </AgGridReact>
-
+        <div>
+        <Snackbar
+          open={open}
+          autoHideDuration={6000}
+          onClose={handleClose}
+          message="Customer deleted"
+        />
+      </div>
     
     </div>
     </div>
